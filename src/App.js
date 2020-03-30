dcc6a3d2767463c2bde9abe4d2924c45516ec5f9
Teresa Malave <dev.malave7@gmail.com>
2020-03-30 04:14:53 -0500

    addition of headers for each image channel

--- src/App.js
+++ src/App.js
@@ -19,15 +19,44 @@ export default class App extends Component {
         <p>TUNE INTO <em>NAUTILUS'</em> DEEP DIVE PORTALS</p>
 
         <div id='channels-container'>
+          <h3></h3>
+
           <img src= { bioImage } alt='bio_and_beyond' className='channel-img'/>
+
+          <h3></h3>
+
           <img src= { femaleFaceImg } alt='artistic_female_face' className='channel-img'/> 
+
+          <h3></h3>
+
           <img src= { numberedBoxImg } alt='numbered_slide_puzzle_game' className='channel-img'/> 
+
+          <h3></h3>
+
           <img src= { natureImg } alt='watercolor_nature_painting' className='channel-img'/>
+
+          <h3></h3>
+
           <img src= { grillingHotDogImg } alt='grilling_Hotdog_image' className='channel-img'/>
+
+          <h3></h3>
+
           <img src= { cosmosImg } alt='cosmos_in_blackAndGold_image' className='channel-img'/>
+
+          <h3></h3>
+
           <img src= { poetryImg } alt='pond_grainfields_image' className='channel-img'/>
+
+          <h3></h3>
+
           <img src= { birthdayCakeImg } alt='aging_bdayCake_image' className='channel-img'/>
+
+          <h3></h3>
+
           <img src= { nightSkyImg } alt='clear_nightSkylight_image' className='channel-img'/>
+
+          <h3></h3>
+          
           <img src= { neuroImg } alt='abstract_neuro_img' className='channel-img'/>
         </div>  
 
