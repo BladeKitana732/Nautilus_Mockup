7cbde4491a8d048d6def720fe358abc7a680e48a
Teresa Malave <dev.malave7@gmail.com>
2020-03-30 03:29:45 -0500

    img test 2 due to compiler warnings for not having alt

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
-import bioImage from './images/bio_and_beyond.jpeg'
+import bioImage from './images/bio_and_beyond.jpeg';
+import femaleFaceImg from './images/wise_sci_and_engineering.png';
 
 
 export default class App extends Component {
@@ -8,8 +9,8 @@ export default class App extends Component {
     return (
       <div>
           <img src= { bioImage } alt='bio_and_beyond'/>
+          <img src= { femaleFaceImg } alt='artistic_female_face' /> 
           {/* <img src= {}/> 
-          <img src= {}/> 
           <img src= {}/>
           <img src= {}/>
           <img src= {}/>
