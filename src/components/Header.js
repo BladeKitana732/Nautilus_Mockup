dfe6159efe6201865bb53601d5cb52ae7351001b
Teresa Malave <dev.malave7@gmail.com>
2020-03-30 22:43:47 -0500

    adding play button img for TLAS channel img section

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,6 +9,7 @@ import poetryImg from './images/poetry.jpeg';
 import birthdayCakeImg from './images/aging.png';
 import nightSkyImg from './images/alliance.png';
 import neuroImg from './images/maxplanck.jpeg';
+import play from './images/playButton.png'
 
 export default class Header extends Component {
     render() {
@@ -16,39 +17,39 @@ export default class Header extends Component {
             <div id='channels-container'>
 
                <article> 
-                <h2>Biology+Beyond</h2>
+                <h2>BIOLOGY+BEYOND</h2>
                 <img src= { bioImage } alt='bio_and_beyond' className='channel-img'
-                /><h4>sdajks</h4>
-                <p>ands</p>
+                /><h4>Making Sense of the Genome, at Last</h4>
+                <p>by Adam Piore</p>
                </article>
 
                <article>
-                <h2>Biology+Beyond</h2>
+                <h2>Woman in Science & Engineering</h2>
                 <img src= { femaleFaceImg } alt='artistic_female_face' className='channel-img'/> 
-                <h4>jfsk</h4>
-                <p>jasjdasd</p>
+                <h4>Your Brain Is On the Brink of Chaos</h4>
+                <p>by Kelly Clancy</p>
                </article>
 
 
                <article> 
-                <h2>Biology+Beyond</h2>
+                <h2>QUANTA ABSTRACTIONS</h2>
                 <img src= { numberedBoxImg } alt='numbered_slide_puzzle_game' className='channel-img'/> 
-                <h4>jfsk</h4>
-                <p>jasjdasd</p>
+                <h4>Mathematicians Calculate How Randomness Creeps In</h4>
+                <p>by Marcos Woo</p>
                 </article>
 
                <article>
-                <h2>Biology+Beyond</h2>
+                <h2>EARTH</h2>
                 <img src= { natureImg } alt='watercolor_nature_painting' className='channel-img'/>
-                <h4>jfsk</h4>
-                <p>jasjdasd</p>
+                <h4>The Deep Time of Walden Pond</h4>
+                <p>by Curt Stager</p>
                 </article> 
 
                <article> 
-                <h2>Biology+Beyond</h2>
+                <h2>THINK LIKE A SCIENTIST</h2>
                 <img src= { grillingHotDogImg } alt='grilling_Hotdog_image' className='channel-img'/>
-                <h4>jfsk</h4>
-                <p>jasjdasd</p>
+                <h4><img src= { play } alt='play_button' id='play-button'/>The Best Burger Place Is a Lab</h4>
+                <p>by Thomas King</p>
                </article>
 
                <article>
